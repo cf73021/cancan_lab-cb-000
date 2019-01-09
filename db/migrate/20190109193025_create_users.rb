04652cf0a4d25312269848fd86693b1d5960585e
Chris Franklin <cf73021@gmail.com>
2019-01-09 20:06:46 +0000

    Automatically backed up by Learn

--- db/migrate/20190109193025_create_users.rb
+++ db/migrate/20190109193025_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.text :name
+      t.datetime :created_at, null: false
+      t.datetime :updated_at, null: false
       t.timestamps
     end
   end
