e662067bce134711dc1b59c7a6e2bb4b1d5d09ee
Chris Franklin <cf73021@gmail.com>
2019-01-09 19:47:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190109193626) do
-
-  create_table "notes", force: :cascade do |t|
-    t.text    "content"
-    t.integer "user_id"
-  end
+ActiveRecord::Schema.define(version: 20190109194428) do
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "viewers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
